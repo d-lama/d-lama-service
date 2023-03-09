8d092d5c303d119f5c7b0adddf5370195c291d10
grandjoe <grandjoe@students.zhaw.ch>
2023-03-09 14:07:02 +0100

    API setup
    - Setup Repository-Pattern with generic repository
    - Setup UnitOfWork-Pattern
    - Create first example entity
    - UnitOfWork uses DI

---
+++ D-lama-service/d-lama-service/Repositories/Core/IRepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq.Expressions;
+using Data;
+
+namespace d_lama_service.Repositories.Core
+{
+    /// <summary>
+    /// Generic repository interface.
+    /// </summary>
+    /// <typeparam name="TEntity"> The entity type. </typeparam>
+    public interface IRepository<TEntity> where TEntity : Entity
+    {
+        /// <summary>
+        /// Gets an entity by a provided id.
+        /// </summary>
+        /// <param name="id"> The id of the entity. </param>
+        /// <returns> The entity. </returns>
+        Task<TEntity?> GetAsync(int id);
+
+        /// <summary>
+        /// Gets all entities.
+        /// </summary>
+        /// <returns> All entities. </returns>
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        /// <summary>
+        /// Gets all entities that matches the predicated.
+        /// </summary>
+        /// <param name="predicate"> The predicate the entities need to match. </param>
+        /// <returns> All entities which have matched the predicate. </returns>
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+
+        /// <summary>
+        /// Adds or updates and entity.
+        /// </summary>
+        /// <param name="entity"> The entity to add or update. </param>
+        void Update(TEntity entity);
+
+        /// <summary>
+        /// Adds or updates a range of entities.
+        /// </summary>
+        /// <param name="entities"> The entities to add or update. </param>
+        void UpdateRange(IEnumerable<TEntity> entities);
+
+        /// <summary>
+        /// Deletes an entity.
+        /// </summary>
+        /// <param name="entity">The entity to delete. </param>
+        void Delete(TEntity entity);
+
+        /// <summary>
+        /// Deletes a range of entities. 
+        /// </summary>
+        /// <param name="entities"> The entities to delete. </param>
+        void DeleteRange(IEnumerable<TEntity> entities);
+    }
+}
---
+++ D-lama-service/d-lama-service/Repositories/Core/Repository.cs
@@ -0,0 +1,60 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace d_lama_service.Repositories.Core
+{
+    /// <summary>
+    /// The generic repository. 
+    /// </summary>
+    /// <typeparam name="TEntity"> The entity type. </typeparam>
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity
+    {
+        protected readonly DbContext Context;
+        protected DbSet<TEntity> Entities;
+
+        /// <summary>
+        /// Constructor of the Repository.
+        /// </summary>
+        /// <param name="context"> The whole DB context. </param>
+        public Repository(DbContext context) 
+        {
+            Context = context;
+            Entities = Context.Set<TEntity>();
+        }
+        
+        public async Task<TEntity?> GetAsync(int id) 
+        {
+            return await Entities.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await Entities.ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await Entities.Where(predicate).ToListAsync();
+        }
+
+        public void Update(TEntity entity)
+        {
+            Entities.Update(entity); 
+        }
+
+        public void UpdateRange(IEnumerable<TEntity> entities)
+        {
+            Entities.UpdateRange(entities);
+        }
+        public void Delete(TEntity entity)
+        {
+            Entities.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<TEntity> entities)
+        {
+            Entities.RemoveRange(entities);
+        }
+    }
+}
